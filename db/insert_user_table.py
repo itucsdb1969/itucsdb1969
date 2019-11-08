435706a7cd3ac54baee44c6356f15efef578d672
isydmr <i.sydmr@gmail.com>
2019-11-08 15:06:49 +0000

    Add user table

--- db/insert_user_table.py
+++ db/insert_user_table.py
@@ -2,13 +2,13 @@ import psycopg2 as dbapi2
 import db.get_db_url as db_url
 
 
-def insert_players_db(player):
-    query = "INSERT INTO PLAYERS (NAME, GENRE, DURATION_IN_SECONDS, SINGER, YEAR) VALUES(%s, %s, %s, %s, %s)"
+def insert_users_db(user):
+    query = "INSERT INTO USERS (user_id, name, password, is_active, is_admin) VALUES(%s, %s, %s, %s, %s)"
     url = db_url.get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
-        cursor.execute(query, (player.name, player.team_id, player.rating,
-                               player.rating, player.is_available))
+        cursor.execute(query, (user.id, user.name, user.password,
+                               user.is_active, user.is_admin))
         users = cursor.fetchall()
         cursor.close()
         return users
