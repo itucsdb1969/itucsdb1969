1950591e1cb86ac5a5a8a0d944b610341a0e15ab
isydmr <i.sydmr@gmail.com>
2019-11-09 15:48:55 +0000

    Edit variable names

--- db/get_user_id.py
+++ db/get_user_id.py
@@ -3,7 +3,7 @@ import db.get_db_url as db_url
 
 
 def get_user_id_with_username(user_name):
-    query = "SELECT user_id FROM Users WHERE user_name = %s"
+    query = "SELECT user_id FROM Users WHERE name = %s"
     url = db_url.get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
