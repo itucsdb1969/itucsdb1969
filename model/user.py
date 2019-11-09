de76f23d4d07bb799deb4883b498b1c01112c476
isydmr <i.sydmr@gmail.com>
2019-11-09 17:39:13 +0000

    Add models

---
+++ model/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, name, password, is_active, is_admin):
+        self.user_id = user_id
+        self.name = name
+        self.password = password
+        self.is_active = is_active
+        self.is_admin = is_admin
+
