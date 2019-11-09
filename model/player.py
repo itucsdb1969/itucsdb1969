fce76c64265167712d0338c09378f6000355bc53
isydmr <i.sydmr@gmail.com>
2019-11-09 18:08:50 +0000

    Edit player model

--- model/player.py
+++ model/player.py
@@ -1,6 +1,5 @@
-class Team:
-    def __init__(self, user_id, name, rating, age):
-        self.user_id = user_id
+class Player:
+    def __init__(self, name, rating, age):
         self.name = name
         self.rating = rating
         self.age = age
