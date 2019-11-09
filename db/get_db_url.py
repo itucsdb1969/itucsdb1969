3f94fcc6036a0e969e9b342186869cd5f78435a2
hhuseyinkacmaz <huseyinkacmaz14@gmail.com>
2019-11-09 19:06:40 +0300

    Fix bugs

--- db/get_db_url.py
+++ db/get_db_url.py
@@ -3,9 +3,9 @@ import sys
 
 def get_db_url():
     url = os.getenv("DATABASE_URL")
-    url = "dbname='postgres' user='postgres' host='localhost' password='123456'"
-    return url
+    #url = "dbname='postgres' user='postgres' host='localhost' password='123456'"
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
+    return url
     
