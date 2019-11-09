447e43b302006d2eb198b11f55a38471cce85e16
isydmr <i.sydmr@gmail.com>
2019-11-09 20:17:18 +0000

    Edit playet query

--- db/update_player_table.py
+++ db/update_player_table.py
@@ -7,7 +7,7 @@ import db.get_team_id as db_team
 def update_players_db(player, user_name, team_name):
     user_id = db_usr.get_user_id_with_username(user_name)
     team_id = db_team.get_team_id_with_teamname(team_name)
-    query = "UPDATE Player SET (name, age, team_id ) VALUES(%s, %s, %s ) WHERE user_id = %s"
+    query = "UPDATE Player SET name = %s, age = %s, team_id = %s WHERE user_id = %s"
     url = db_url.get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
