00be2f4da9baf62c8c143e707e54e721a9e86e54
isydmr <i.sydmr@gmail.com>
2019-11-09 17:58:08 +0000

    Add user pw query

--- db/get_user_pw.py
+++ db/get_user_pw.py
@@ -2,7 +2,7 @@ import psycopg2 as dbapi2
 import db.get_db_url as db_url
 
 
-def get_user_id_with_username(user_name):
+def get_user_pw_with_username(user_name):
     query = "SELECT password FROM Users WHERE name = %s"
     url = db_url.get_db_url()
     with dbapi2.connect(url) as connection:
