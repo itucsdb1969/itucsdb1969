cedd2bbe01bf26b6c326d82126b1132961634c24
isydmr <i.sydmr@gmail.com>
2019-12-14 15:32:07 +0300

    Edit team model

--- model/match.py
+++ model/match.py
@@ -1,6 +1,5 @@
 class Match:
-    def __init__(self, team1_id, team2_id):
-        self.team1_id = team1_id
-        self.team2_id = team2_id
-
-
+    def __init__(self, team1_name, team2_name, match_id=0):
+        self.match_id = match_id
+        self.team1_name = team1_name
+        self.team2_name = team2_name
