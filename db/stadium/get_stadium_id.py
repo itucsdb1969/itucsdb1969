8bf7c96eaefaaca35bb8f2ea6591937f0c33c66c
isydmr <i.sydmr@gmail.com>
2019-12-14 16:50:08 +0300

    Add stadium id func.

---
+++ db/stadium/get_stadium_id.py
@@ -0,0 +1,12 @@
+import psycopg2 as dbapi2
+from db.utils.get_db_url import get_db_url
+
+def get_stad_id_with_stad_name(stadium):
+    query = "SELECT stadium_id FROM Stadium WHERE name = %s"
+    url = get_db_url()
+    with dbapi2.connect(url) as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, (stadium.name,))
+        stad_id = cursor.fetchone()
+        cursor.close()
+        return stad_id
--- db/stadium/update_stadium_table.py
+++ db/stadium/update_stadium_table.py
@@ -1,8 +1,5 @@
 import psycopg2 as dbapi2
 from db.utils.get_db_url import get_db_url
-from db.user.get_user_id import get_user_id_with_username
-from db.team.get_team_id import get_team_id_with_teamname
-
 
 def update_players_db(stadium, stadium_new):
     query = "UPDATE Stadium SET name = %s WHERE name = %s"
