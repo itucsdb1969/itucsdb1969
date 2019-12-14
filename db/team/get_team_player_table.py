c0bc58e7c80cfb62153b161f24f3a00d40af422e
isydmr <i.sydmr@gmail.com>
2019-12-14 14:45:54 +0300

    Add db func. for teams pages

--- db/team/get_team_player_table.py
+++ db/team/get_team_player_table.py
@@ -3,12 +3,12 @@ from db.utils.get_db_url import get_db_url
 
 
 def get_team_players_with_team_id(team_id):
-    query = "SELECT distinct p.name FROM Team as t, Player as p" \
+    query = "SELECT distinct p.name, p.rating, p.age FROM Team as t, Player as p" \
             "WHERE p.team_id = %s"
     url = get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
         cursor.execute(query, (team_id, ))
-        player_names = cursor.fetchone()
+        players = cursor.fetchone()
         cursor.close()
-        return player_names
+        return players
