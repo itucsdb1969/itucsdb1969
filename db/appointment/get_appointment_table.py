e02101b220cdd670ca80fe34954309e5830d1c9f
isydmr <i.sydmr@gmail.com>
2019-12-14 17:25:57 +0300

    Refactor app db

--- db/appointment/get_appointment_table.py
+++ db/appointment/get_appointment_table.py
@@ -2,7 +2,7 @@ import psycopg2 as dbapi2
 from db.utils.get_db_url import get_db_url
 
 
-def get_teams_db():
+def get_appointments_db():
     query = "SELECT * FROM APPOINTMENT"
     url = get_db_url()
     with dbapi2.connect(url) as connection:
