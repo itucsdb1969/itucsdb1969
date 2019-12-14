fb1dcb1fb617260b834fa12f17a15f21987de30d
isydmr <i.sydmr@gmail.com>
2019-12-14 14:41:50 +0300

    Add db func. for teams page

--- db/team/get_team_player_table.py
+++ db/team/get_team_player_table.py
@@ -2,14 +2,13 @@ import psycopg2 as dbapi2
 from db.utils.get_db_url import get_db_url
 
 
-def get_team_players_with_team_id(team_name):
+def get_team_players_with_team_id(team_id):
     query = "SELECT distinct p.name FROM Team as t, Player as p" \
             "WHERE p.team_id = %s"
     url = get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
-        cursor.execute(query, (team_name, ))
-        team_id = cursor.fetchone()
+        cursor.execute(query, (team_id, ))
+        player_names = cursor.fetchone()
         cursor.close()
-        print("team_id: ", team_id)
-        return team_id
+        return player_names
