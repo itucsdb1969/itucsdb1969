43903d3d86e73420bf131e89ea2e5601aff057ae
isydmr <i.sydmr@gmail.com>
2019-12-15 17:31:27 +0300

    Update appointment query

--- db/appointment/update_appointment_table.py
+++ db/appointment/update_appointment_table.py
@@ -1,13 +1,17 @@
 import psycopg2 as dbapi2
 from db.utils.get_db_url import get_db_url
+from db.user.get_user_id import get_user_id_with_username
 
 
-
-def update_appointments_db(appointment, appointment_name):
-    query = "UPDATE APPOINTMENT SET name = %s  match_id= %s stadium_id = %s start_time = %s end_time = %s WHERE name = %s"
+def update_appointments_db(appointment_id, username):
+    user_id = get_user_id_with_username(username)
+    query = """	UPDATE MATCH SET
+                 team2_id = (select team_id from player where user_id = %s)
+                 WHERE match_id = (select match_id from APPOINTMENT
+                 where appointment_id = %s)"""
     url = get_db_url()
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
-        cursor.execute(query, (appointment.name, appointment.match_id, appointment.stadium_id. appointment.start_time, appointment.end_time, appointment_name))
+        cursor.execute(query, (appointment_id, user_id, ))
         cursor.close()
         return True
