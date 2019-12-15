5ca018f651bae5817bf6dec72f3f2347f3197720
isydmr <i.sydmr@gmail.com>
2019-12-15 12:52:20 +0300

    Fix server config

--- db/utils/get_db_url.py
+++ db/utils/get_db_url.py
@@ -4,7 +4,7 @@ import sys
 
 def get_db_url():
     url = os.getenv("DATABASE_URL")
-    url = "dbname='itucsdb1969' user='postgres' host='localhost' password='postgres'"
+    # url = "dbname='itucsdb1969' user='postgres' host='localhost' password='postgres'"
     # url = "dbname='postgres' user='postgres' host='localhost' password='123456'"
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
