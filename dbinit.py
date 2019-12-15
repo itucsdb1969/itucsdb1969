58db7f3a96dd4d259ab458e81d6f086603a5bdb9
isydmr <i.sydmr@gmail.com>
2019-12-15 19:43:29 +0300

    Fix db config

--- dbinit.py
+++ dbinit.py
@@ -90,8 +90,8 @@ def initialize(url):
 
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
-    url = "dbname='itucsdb1969' user='postgres' host='localhost' password='postgres'"
-    #url = "dbname='postgres' user='postgres' host='localhost' password='123456'"
+    # url = "dbname='itucsdb1969' user='postgres' host='localhost' password='postgres'"
+    # url = "dbname='postgres' user='postgres' host='localhost' password='123456'"
     initialize(url)
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
--- server.py
+++ server.py
@@ -1,30 +1,28 @@
+import psycopg2
 from flask import Flask, request, session, flash
 from flask import render_template, redirect
-from db.team.get_team_table import get_teams_db
-from db.player.insert_player_table import insert_players_db
-from db.player.get_player_table import get_players_db
-from db.user.insert_user_table import insert_users_db
-from db.user.get_user_pw import get_user_pw_with_username
-from db.player.update_player_table import update_players_db
-from db.utils.get_profile_status import check_profile_exists
-from db.player.delete_player_table import delete_players_db
 from model.user import User
 from model.player import Player
 from model.team import Team
 from model.match import Match
 from model.appointment import Appointment
-from db.appointment.insert_appointment_table import insert_appointments_db
-from db.team.get_team_id import get_team_id_with_teamname
+from db.user.insert_user_table import insert_users_db
+from db.user.get_user_pw import get_user_pw_with_username
+from db.utils.get_profile_status import check_profile_exists
+from db.match.insert_match_table import insert_match_db
+from db.player.insert_player_table import insert_players_db
+from db.player.get_player_table import get_players_db
+from db.player.update_player_table import update_players_db
+from db.player.delete_player_table import delete_players_db
+from db.player.get_user_player_table import get_player_with_username
+from db.team.get_team_table import get_teams_db
 from db.team.get_team_player_table import get_team_players_with_team_id
 from db.team.insert_team_table import insert_teams_db
-from db.match.insert_match_table import insert_match_db
-from db.match.get_match_table import get_match_db
 from db.stadium.get_stadium_table import get_stadiums_db
 from db.stadium.get_stadium_id import get_stad_id_with_stad_name
+from db.appointment.insert_appointment_table import insert_appointments_db
 from db.appointment.get_appointment_table import get_appointments_db
 from db.appointment.update_appointment_table import update_appointments_db
-from db.player.get_user_player_table import get_player_with_username
-import psycopg2
 
 app = Flask(__name__)
 app.secret_key = 'ITUCSDB1969'
